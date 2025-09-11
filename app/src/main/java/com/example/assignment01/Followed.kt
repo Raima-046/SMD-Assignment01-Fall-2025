e993ab344b37a944af44874b1f0f0bf4ea47f282
Raima Tariq <rymatariq@gmail.com>
2025-09-12 02:08:17 +0500

    Story Pages

--- app/src/main/java/com/example/assignment01/Followed.kt
+++ app/src/main/java/com/example/assignment01/Followed.kt
@@ -1,6 +1,8 @@
 package com.example.assignment01
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,12 @@ class Followed : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_followed)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        var follow = findViewById<TextView>(R.id.fol)
+
+        follow.setOnClickListener {
+            var intent = Intent(this, Follow::class.java)
+            startActivity(intent)
         }
     }
 }
